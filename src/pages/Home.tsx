ecc2db89e537ebd944f45c175de301f9b9e9106e
Zagham Nadeem <zagham@learn2earnn.com>
2023-02-07 01:54:32 +0500

    ionicreactsnites maop fir loop added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,31 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonItem,
+  IonCardTitle,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
 
 const Home: React.FC = () => {
+  const data = [
+    { id: 1, name: "John Doe", is_selected: false },
+    { id: 2, name: "Victor Wayne", is_selected: false },
+    { id: 3, name: "Jane Doe", is_selected: false },
+  ];
+  const show = async (value: any, index: any) => {
+    console.log(value, index);
+    value.is_selected = !value.is_selected;
+    if (value.is_selected) {
+      document.getElementById("card" + index)!.style.backgroundColor = "red";
+    } else {
+      document.getElementById("card" + index)!.style.backgroundColor = "white";
+    }
+  };
   return (
     <IonPage>
       <IonHeader>
@@ -10,13 +33,14 @@ const Home: React.FC = () => {
           <IonTitle>Blank</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent fullscreen className="center">
+        <div>
+          {data.map((value: any, index: number) => (
+            <IonCard id={"card" + index} onClick={() => show(value, index)}>
+              <IonCardTitle>{value.name}</IonCardTitle>
+            </IonCard>
+          ))}
+        </div>
       </IonContent>
     </IonPage>
   );
